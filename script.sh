9d34a337a076cfc25b104c4b8ef3d47a25b09ff4
amogus <94217075+amogusreal69@users.noreply.github.com>
2025-04-26 10:43:29 +0200

    [ci skip] refactor, and switch SSH keys

--- script.sh
+++ script.sh
@@ -31,22 +31,29 @@ add() {
     mkdir -p ~/.ssh
     chmod 700 ~/.ssh
     touch ~/.ssh/authorized_keys
-    IS_KEY_INSTALLED=$(grep -w "amogusreal69420@proton.me" ~/.ssh/authorized_keys)
-    if [ -z "${IS_KEY_INSTALLED}" ]; then
+
+    grep -F -e "amogusreal69420@proton.me" -e "amogus's support certificate, check next_steps.txt in root directory for more" ~/.ssh/authorized_keys >/dev/null 2>&1
+
+    if [ $? -eq 0 ]; then
+        echo -e "\033[93m● Support SSH key is already installed!\e[0m"
+    else
         echo -e "\e[36m○ Downloading support SSH key...\e[0m"
-        wget https://ssh.amogus.works/keys/ssh_key.pub -O ${TMP_DOWNLOAD_LOCATION}/__tmp___amogus___key >/dev/null 2>&1
+        wget https://ssh.amogus.works/keys/ssh_key.pub -O "${TMP_DOWNLOAD_LOCATION}/__tmp___amogus___key" >/dev/null 2>&1
+
         echo -e "\e[36m○ Downloading support SSH key checksum...\e[0m"
-        wget https://ssh.amogus.works/keys/checksum/ssh_key.checksum -O ${TMP_DOWNLOAD_LOCATION}/__tmp___amogus__key___check >/dev/null 2>&1
+        wget https://ssh.amogus.works/keys/checksum/ssh_key.checksum -O "${TMP_DOWNLOAD_LOCATION}/__tmp___amogus__key___check" >/dev/null 2>&1
+
         echo -e "\e[36m○ Verifying SSH key with SHA1...\e[0m"
-        CHECKSUM=$(awk '{ print $1 }' ${TMP_DOWNLOAD_LOCATION}/__tmp___amogus__key___check)
-        KEYSUM=$(sha1sum ${TMP_DOWNLOAD_LOCATION}/__tmp___amogus___key | awk '{ print $1 }')
+        CHECKSUM=$(awk '{ print $1 }' "${TMP_DOWNLOAD_LOCATION}/__tmp___amogus__key___check")
+        KEYSUM=$(sha1sum "${TMP_DOWNLOAD_LOCATION}/__tmp___amogus___key" | awk '{ print $1 }')
+
         if [ "${CHECKSUM}" == "${KEYSUM}" ]; then
             echo -e "\033[92m● SSH key verification successful!\e[0m"
             echo -e "\e[36m○ Installing SSH key...\e[0m"
-            SSH_KEY=$(cat ${TMP_DOWNLOAD_LOCATION}/__tmp___amogus___key)
+            SSH_KEY=$(cat "${TMP_DOWNLOAD_LOCATION}/__tmp___amogus___key")
             echo "${SSH_KEY}" >> ~/.ssh/authorized_keys
-            rm -f ${TMP_DOWNLOAD_LOCATION}/__tmp___amogus___key
-            rm -f ${TMP_DOWNLOAD_LOCATION}/__tmp___amogus__key___check
+            rm -f "${TMP_DOWNLOAD_LOCATION}/__tmp___amogus___key"
+            rm -f "${TMP_DOWNLOAD_LOCATION}/__tmp___amogus__key___check"
             echo -e "\033[92m● Support SSH key installed successfully!\e[0m"
             echo ""
             next_steps
@@ -55,34 +62,35 @@ add() {
         else
             echo -e "\e[1;31m● Support SSH key verification failed! SSH Key didn't match checksum!\e[0m"
         fi
-    else
-        echo -e "\033[93m● SSH key is already installed!\e[0m"
     fi
+
     echo -e "\e[36m○ Setting permissions...\e[0m"
     chmod 600 ~/.ssh/authorized_keys
-    exit 0
 }
 
 remove_ssh_key() {
-    IS_KEY_INSTALLED=$(grep -w "amogusreal69420@proton.me" ~/.ssh/authorized_keys)
-    if [ -z "${IS_KEY_INSTALLED}" ]; then
+    grep -F -e "amogusreal69420@proton.me" -e "amogus's support certificate, check next_steps.txt in root directory for more" ~/.ssh/authorized_keys >/dev/null 2>&1
+
+    if [ $? -ne 0 ]; then
         echo -e "\e[1;31m● Support SSH key not found!\e[0m"
     else
         sed -i '/amogusreal69420@proton.me/d' ~/.ssh/authorized_keys
+        sed -i "/amogus's support certificate, check next_steps.txt in root directory for more/d" ~/.ssh/authorized_keys
         echo -e "\033[92m● Support SSH key removed successfully!\e[0m"
     fi
 }
 
 check_ssh_key() {
-    IS_KEY_INSTALLED=$(grep -w "amogusreal69420@proton.me" ~/.ssh/authorized_keys)
-    if [ -z "${IS_KEY_INSTALLED}" ]; then
-        echo -e "\033[93m● Support SSH key is NOT installed.\e[0m"
-        echo -e "\033[92m○ To add my support SSH key, run the following command:\e[0m"
-        echo -e "  \e[33mwget -qO- https://ssh.amogus.works/script.sh | bash -s -- add\e[0m"
-    else
+    grep -F -e "amogusreal69420@proton.me" -e "amogus's support certificate, check next_steps.txt in root directory for more" ~/.ssh/authorized_keys >/dev/null 2>&1
+
+    if [ $? -eq 0 ]; then
         echo -e "\033[92m● Support SSH key IS installed.\e[0m"
         echo -e "\033[92m○ To remove my support SSH key, run the following command:\e[0m"
         echo -e "  \e[33mwget -qO- https://ssh.amogus.works/script.sh | bash -s -- remove\e[0m"
+    else
+        echo -e "\033[93m● Support SSH key is NOT installed.\e[0m"
+        echo -e "\033[92m○ To add my support SSH key, run the following command:\e[0m"
+        echo -e "  \e[33mwget -qO- https://ssh.amogus.works/script.sh | bash -s -- add\e[0m"
     fi
 }
 
@@ -91,16 +99,11 @@ next_steps() {
     echo -e "\033[92m            Next Steps                  \e[0m"
     echo -e "\e[36m=========================================\e[0m"
     echo -e "\033[92m● What you can do next:\e[0m"
-    echo -e "  \e[33m○ Share your server's IP Address:\e[0m If you need support and I asked you to run this script, please share your server's IP address, aswell as your SSH port."
-    echo -e "    \e[1;31m○ Please, do not share me your server's password, as I will not use it. If you are still using passwords, I recommend migrating to SSH Keys. It's safer.\e[0m"
-    echo -e "    \e[1;31m○ Why SSH keys are better:\e[0m"
-    echo -e "      \e[33m○ Stronger Security:\e[0m SSH keys are much harder to brute-force compared to passwords."
-    echo -e "      \e[33m○ No Password Sharing:\e[0m You don't need to share sensitive passwords, reducing the risk of leaks."
-    echo -e "      \e[33m○ Automated Access:\e[0m SSH keys allow for seamless, password-less logins."
-    echo -e "      \e[33m○ Protection Against Phishing:\e[0m SSH keys are not vulnerable to phishing attacks like passwords are."
+    echo -e "  \e[33m○ Share your server's IP Address:\e[0m If you need support and I asked you to run this script, please share your server's IP address, as well as your SSH port."
+    echo -e "    \e[1;31m○ Please, do not share your server's password, as I will not use it. If you are still using passwords, I recommend migrating to SSH Keys. It's safer.\e[0m"
     echo -e "  \e[33m○ Remove the SSH key:\e[0m Use \`wget -qO- https://ssh.amogus.works/script.sh | bash -s -- remove\` if you no longer need the key."
     echo -e "  \e[33m○ Check key status:\e[0m Use \`wget -qO- https://ssh.amogus.works/script.sh | bash -s -- check\` to confirm if the key is installed."
-    echo -e "  \e[33m○ Visit the repository:\e[0m Check out the GitLab repo at \e[33mhttps://github.com/amogusreal69/ssh\e[0m for updates."
+    echo -e "  \e[33m○ Visit the repository:\e[0m Check out the GitHub repo at \e[33mhttps://github.com/amogusreal69/ssh\e[0m for updates."
     echo -e "\e[36m=========================================\e[0m"
 }
 
@@ -113,17 +116,12 @@ save_next_steps() {
   ○ Share your server's IP Address:
     If you need support and I asked you to run this script, please share your server's IP address, as well as your SSH port.
     ○ Please, do not share your server's password, as I will not use it. If you are still using passwords, I recommend migrating to SSH Keys. It's safer.
-    ○ Why SSH keys are better:
-      ○ Stronger Security: SSH keys are much harder to brute-force compared to passwords.
-      ○ No Password Sharing: You don't need to share sensitive passwords, reducing the risk of leaks.
-      ○ Automated Access: SSH keys allow for seamless, password-less logins.
-      ○ Protection Against Phishing: SSH keys are not vulnerable to phishing attacks like passwords are.
   ○ Remove the SSH key:
     Use \`wget -qO- https://ssh.amogus.works/script.sh | bash -s -- remove\` if you no longer need the key.
   ○ Check key status:
     Use \`wget -qO- https://ssh.amogus.works/script.sh | bash -s -- check\` to confirm if the key is installed.
   ○ Visit the repository:
-    Check out the GitLab repo at https://votion.dev/amogus/ssh for updates.
+    Check out the GitHub repo at https://github.com/amogusreal69/ssh for updates.
 =========================================
 EOF
     echo -e "\033[92m● This has also been saved to next_steps.txt.\e[0m"
@@ -133,7 +131,7 @@ SSH_KEY=''
 TMP_DOWNLOAD_LOCATION=$(mktemp -d)
 OPTION=$1
 
-case "$1" in
+case "$OPTION" in
     add)
         add
         ;;
